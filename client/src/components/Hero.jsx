724e1ad121756591d8b805cf5d5c36eff0da21ac
Shlok Maktarpara <shlokmaktarpara023@gmail.com>
2025-12-19 19:05:10 +0530

    feat: create CarCard component structure

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         Luxury Cars On Rent
       </h1>
 
-      <form className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-1g md: rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]">
+      <form className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-1g md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]">
         <div className="flex flex-col md:flex-row items-start md:items-center gap-10 md:ml-8">
           <div className="flex flex-col items-start gap-2">
             <select
