fd67d4fdc845a7baabe048e1093c7aaa158edc2d
Shlok Maktarpara <shlokmaktarpara023@gmail.com>
2025-12-20 20:43:20 +0530

    feat: add Newsletter section in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import CTA from '../components/CTA'
 import Testimonial from '../components/Testimonial'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <FeaturedSection />
         <CTA />
         <Testimonial />
+        <Newsletter />
     </div>
   )
 }
