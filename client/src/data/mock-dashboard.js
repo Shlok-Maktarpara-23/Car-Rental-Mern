2a9ddf1f3bbbd9a8bd09137993cb15b3395990e5
Shlok Maktarpara <shlokmaktarpara023@gmail.com>
2025-12-24 14:14:10 +0530

    feat: add dummy data's for booking cars, cars, dashboard, user Data

---
+++ client/src/data/mock-dashboard.js
@@ -0,0 +1,11 @@
+export const dummyDashboardData = {
+    "totalCars": 4,
+    "totalBookings": 2,
+    "pendingBookings": 0,
+    "completedBookings": 2,
+    "recentBookings": [
+        dummyMyBookingsData[0],
+        dummyMyBookingsData[1]
+    ],
+    "monthlyRevenue": 840
+}
