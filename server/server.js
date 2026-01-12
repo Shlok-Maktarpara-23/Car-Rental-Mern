9c81013e2a1da636d3323af85921d87418efccca
Shlok Maktarpara <shlokmaktarpara023@gmail.com>
2026-01-12 17:58:40 +0530

    feat: create user model and test users api endpoints

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import connectDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
 
 // Initialize Express app
 const app = express();
@@ -10,13 +11,16 @@ const app = express();
 await connectDB();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 // Sample route
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
+
+// User Routes
+app.use('/api/users', userRouter);
 
 // Start server
 const PORT = process.env.PORT || 3000;
